6439d00130bf7d9c9ef909ffa31ba9854c4e41c6
AlmiraOktinaP <almiraoktinap@gmail.com>
2021-05-29 16:30:05 +0700

    initial commit

---
+++ app/src/main/java/com/example/capstoneproject/ui/dashboard/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.capstoneproject.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.capstoneproject.R;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
