6f991968650712a7c00347dbeafe959731ab0822
CaesarioIga01 <caesarioiga08@gmail.com>
2021-05-30 11:49:34 +0700

    update terbaru

--- app/src/main/java/com/example/capstoneproject/ui/detail/cardetail/CarParkDetailActivity.kt
+++ app/src/main/java/com/example/capstoneproject/ui/detail/cardetail/CarParkDetailActivity.kt
@@ -2,11 +2,17 @@ package com.example.capstoneproject.ui.detail.cardetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.capstoneproject.R
 import com.example.capstoneproject.databinding.ActivityCarParkDetailBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -35,5 +41,25 @@ class CarParkDetailActivity : AppCompatActivity() {
         TabLayoutMediator(tabs, viewPager) { tab, position ->
             tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
+
+        val button: Button = findViewById(R.id.btn_book_car)
+
+        button.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(
+                this@CarParkDetailActivity, R.style.BottomSheetDialogTheme
+            )
+
+            val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
+                R.layout.bottom_sheet,
+                findViewById<LinearLayout>(R.id.bottom_sheet)
+            )
+
+            bottomSheetView.findViewById<View>(R.id.btn_book_detail).setOnClickListener {
+                Toast.makeText(this@CarParkDetailActivity, "Next", Toast.LENGTH_SHORT).show()
+            }
+
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+        }
     }
 }
